fa823277f631de14c4a5552bf3c06e2b19ce9e96
Notebook <Notebook@DESKTOP-QCDM367>
2023-10-20 16:16:51 +0400

    Add project files.

---
+++ WpfChatApp/ChatMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfChatApp
+{
+    public class ChatMessage
+    {
+        public string Sender { get; set; }
+        public string Isender { get; set; }
+    }
+}
